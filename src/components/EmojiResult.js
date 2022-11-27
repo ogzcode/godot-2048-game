7a9b940c93a52b3aaa7fe23b3fb3fba4211c9838
ogzCode <oguzguc44@gmail.com>
2022-11-27 12:55:55 +0300

    Added media queries and converted class based components to functional based components

--- src/components/EmojiResult.js
+++ src/components/EmojiResult.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./EmojiResult.css";
 
-function EmojiRow(props){
+function EmojiRow(props) {
     return (
         <div className='row__box'>
             <span className='symbol'>{props.symbol}</span>
@@ -10,21 +10,16 @@ function EmojiRow(props){
     );
 }
 
-class EmojiResult extends React.Component {
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return (
-            <div className='result__box'>
-                {
-                    this.props.emojies.map(
-                        (emoji, i) => <EmojiRow symbol={emoji.symbol} title={emoji.title} key={i}/>
-                    )
-                }
-            </div>
-        );
-    }
+function EmojiResult(props) {
+    return (
+        <div className='result__box'>
+            {
+                props.emojies.map(
+                    (emoji, i) => <EmojiRow symbol={emoji.symbol} title={emoji.title} key={i} />
+                )
+            }
+        </div>
+    );
 }
 
 export default EmojiResult;
